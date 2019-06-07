f3f9a8c4202576ae32d57b775b83ca6b42fdfbca
Akhilesh L <akhil47@icloud.com>
2019-06-07 22:02:40 +0530

    Dashboard page development

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { BodyComponent } from './body/body.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { TableScrollContainerComponent } from './table-scroll-container/table-scroll-container.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    BodyComponent,
+    DashboardComponent,
+    TableScrollContainerComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/table-scroll-container/table-scroll-container.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-table-scroll-container',
+  templateUrl: './table-scroll-container.component.html',
+  styleUrls: ['./table-scroll-container.component.css']
+})
+export class TableScrollContainerComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
